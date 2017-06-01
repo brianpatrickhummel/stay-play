41ec6edc56f4346e0ec249c9eba6ac9cbe1d95a8
Noe Tellez <noetlz10@gmail.com>
2017-06-01 12:15:39 -0500

    started google api

--- js/DatePicker.js
+++
@@ -1,9 +0,0 @@
-
-	$(document).ready(function() 
-			{
-                //$('#datetimepicker1').datepicker();
-            });
-        
-
-        $("#datepicker-id").getDate();
-        console.log(datepicker-id);
